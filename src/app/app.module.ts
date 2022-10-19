a20b8e6cb9f9c296371ab2d8a124180241ef18a5
João Victor <86169183+KundeVillanova@users.noreply.github.com>
2022-10-19 18:50:50 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { TemplateModule } from './template/template.module';
+import { HomeComponent } from './home/home.component'
+import { ClientsModule } from './clients/clients.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    TemplateModule,
+    ClientsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
