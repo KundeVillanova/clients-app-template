600084ddf8dd0f6ad4c4495bf42c9568aeef0cb7
João Victor <86169183+KundeVillanova@users.noreply.github.com>
2022-10-20 20:51:15 -0300

    Data binding

--- src/app/clients/clients-form/clients-form.component.ts
+++ src/app/clients/clients-form/clients-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Client } from '../client'
+
 
 @Component({
   selector: 'app-clients-form',
@@ -7,7 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientsFormComponent implements OnInit {
 
-  constructor() { }
+  client: Client;
+
+  constructor() { 
+    this.client = new Client();
+     
+  }
+
+  onClick(){
+    console.log(this.client);
+  }
 
   ngOnInit(): void {
   }
